9043fe0a16af9458e8f66bca9b56bffdffaa0561
Olaniyi John <jaolaniyi96@student.lautech.ng>
2023-04-04 20:05:55 +0100

    add

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,9 +11,9 @@ const Footer = () => {
     return (
         <footer className="flex flex-col">
             <div className="w-11/12 md:flex justify-between m-auto items-center">
-                <Image src={Logo} alt="logo image" className='w-7/12 md:w-2/12' />
+                <Image src={Logo} alt="logo image" className='w-7/12 md:w-2/12' data-aos="zoom-in-down" />
 
-                <span className="bg-[#E5E5E5] border border-[#462686] flex items-center w-[120px] h-8 cursor-pointer">
+                <span className="bg-[#E5E5E5] border border-[#462686] flex items-center w-[120px] h-8 cursor-pointer" data-aos="flip-down">
                     <Image src={World} alt="world icon" className='ml-2 cursor-pointer mt-1 mr-2 h-4' />
                     <p>English</p>
                     <Image src={ArrowDown} alt="arrow down" className='ml-2 cursor-pointer mt-1' />
@@ -22,13 +22,13 @@ const Footer = () => {
 
             <div className="md:flex justify-between w-11/12 m-auto mt-10">
                 <div className="w-11/12 md:w-[25%]">
-                    <h2 className="font-normal text-xl text-[#454545] mb-2">No 1 CRM solution in Africa</h2>
-                    <p className="pb-14 font-normal text-lg text-[#01001D99]">Terms of Use | Privacy Policy</p>
+                    <h2 className="font-normal text-xl text-[#454545] mb-2" data-aos="fade-up">No 1 CRM solution in Africa</h2>
+                    <p className="pb-14 font-normal text-lg text-[#01001D99]" data-aos="fade-down">Terms of Use | Privacy Policy</p>
 
-                    <p className="text-[#01001D99] font-normal text-lg mb-2">Get in touch</p>
-                    <h2 className="text-[#333333CC] font-normal text-xl mb-5">+234-80-000-0000</h2>
+                    <p className="text-[#01001D99] font-normal text-lg mb-2" data-aos="fade-up">Get in touch</p>
+                    <h2 className="text-[#333333CC] font-normal text-xl mb-5" data-aos="fade-down">+234-80-000-0000</h2>
 
-                    <div className="flex w-10/12">
+                    <div className="flex w-10/12" data-aos="fade-up">
                         <Image src={FacebookFooter} alt="facebook icon" className='ml-2 cursor-pointer mt-1 mr-2 h-4' />
                         <Image src={TwitterFooter} alt="twitter icon" className='ml-2 cursor-pointer mt-1 mr-2 h-4' />
                         <Image src={LinkedInFooter} alt="linkedin icon" className='ml-2 cursor-pointer mt-1 mr-2 h-4' />
@@ -38,49 +38,49 @@ const Footer = () => {
 
                 <div className="md:flex justify-between w-11/12 md:w-[65%] text-[#01001D99]">
                     <div className="flex flex-col justify-between">
-                        <a href="" className="text-[#150B28] font-bold text-xl pb-5">Product</a>
-                        <a href="">Features</a>
-                        <a href="">Enterprise</a>
-                        <a href="">Security</a>
-                        <a href="">Trust</a>
-                        <a href="">Customer Stories</a>
-                        <a href="">Pricing</a>
-                        <a href="">Integrations</a>
-                        <a href="">Oneburner Partners</a>
+                        <a href="" className="text-[#150B28] font-bold text-xl pb-3 pt-8 md:pt-0" data-aos="fade-up">Product</a>
+                        <a href="" data-aos="fade-down">Features</a>
+                        <a href="" data-aos="fade-down">Enterprise</a>
+                        <a href="" data-aos="fade-down">Security</a>
+                        <a href="" data-aos="fade-down">Trust</a>
+                        <a href="" data-aos="fade-down">Customer Stories</a>
+                        <a href="" data-aos="fade-down">Pricing</a>
+                        <a href="" data-aos="fade-down">Integrations</a>
+                        <a href="" data-aos="fade-down">Oneburner Partners</a>
                     </div>
 
                     <div className="flex flex-col mb-5">
-                        <a href="" className="text-[#150B28] font-bold text-xl pb-5">Company</a>
-                        <a href="">About Us</a>
-                        <a href="">Contact Us</a>
-                        <a href="">Careers</a>
-                        <a href="">In the News</a>
+                        <a href="" className="text-[#150B28] font-bold text-xl pb-3 pt-8 md:pt-0" data-aos="fade-up">Company</a>
+                        <a href="" data-aos="fade-down">About Us</a>
+                        <a href="" data-aos="fade-down">Contact Us</a>
+                        <a href="" data-aos="fade-down">Careers</a>
+                        <a href="" data-aos="fade-down">In the News</a>
                     </div>
 
                     <div className="flex flex-col mb-5">
-                        <a href="" className="text-[#150B28] font-semibold md:font-bold text-lg md:text-xl pb-5">ONEBURNER FOR TEAMS</a>
-                        <a href="">Engineering</a>
-                        <a href="">Financial Services</a>
-                        <a href="">CRM and Sales</a>
-                        <a href="">IT</a>
-                        <a href="">Marketing</a>
-                        <a href="">Customer Support</a>
-                        <a href="">Human Resources</a>
-                        <a href="">Project Management</a>
-                        <a href="">Remote Work</a>
+                        <a href="" className="text-[#150B28] font-semibold md:font-bold text-lg md:text-xl pb-3 pt-8 md:pt-0" data-aos="fade-up">ONEBURNER FOR TEAMS</a>
+                        <a href="" data-aos="fade-down">Engineering</a>
+                        <a href="" data-aos="fade-down">Financial Services</a>
+                        <a href="" data-aos="fade-down">CRM and Sales</a>
+                        <a href="" data-aos="fade-down">IT</a>
+                        <a href="" data-aos="fade-down">Marketing</a>
+                        <a href="" data-aos="fade-down">Customer Support</a>
+                        <a href="" data-aos="fade-down">Human Resources</a>
+                        <a href="" data-aos="fade-down">Project Management</a>
+                        <a href="" data-aos="fade-down">Remote Work</a>
                     </div>
 
                     <div className="flex flex-col">
-                        <a href="" className="text-[#150B28] font-bold text-xl pb-5">RESOURCES</a>
-                        <a href="">Community</a>
-                        <a href="">Guides</a>
-                        <a href="">Templates</a>
-                        <a href="">Video Tutorials</a>
-                        <a href="">Professional Services</a>
-                        <a href="">Knowledge Base</a>
-                        <a href="">Blog</a>
-                        <a href="">Webinars</a>
-                        <a href="">Find a Partner</a>
+                        <a href="" className="text-[#150B28] font-bold text-xl pb-3 pt-8 md:pt-0" data-aos="fade-up">RESOURCES</a>
+                        <a href="" data-aos="fade-down">Community</a>
+                        <a href="" data-aos="fade-down">Guides</a>
+                        <a href="" data-aos="fade-down">Templates</a>
+                        <a href="" data-aos="fade-down">Video Tutorials</a>
+                        <a href="" data-aos="fade-down">Professional Services</a>
+                        <a href="" data-aos="fade-down">Knowledge Base</a>
+                        <a href="" data-aos="fade-down">Blog</a>
+                        <a href="" data-aos="fade-down">Webinars</a>
+                        <a href="" data-aos="fade-down">Find a Partner</a>
                     </div>
                 </div>
             </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 import Logo from "../assets/svg/logo.svg"
 import ArrowDown from "../assets/svg/arrowdown.svg"
 import ArrowRight from "../assets/svg/arrowright.svg"
+import Menu from "../assets/svg/icon-hamburger.svg"
 import Close from "../assets/svg/close.svg"
-import { FaBars } from 'react-icons/fa';
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
 
@@ -15,115 +15,90 @@ const NavBAr = () => {
     }, [navbar])
 
     return (
+        <nav className="w-full flex flex-col md:flex-row justify-between items-center h-20 bg-slate-300 text-black fixed top-0 left-0 right-0 z-10">
 
+            <div className="flex items-center justify-between py-3 md:py-5 md:block w-full px-4 md:w-2/12 h-14 md:h-14">
+                <Link href="/">
+                    <Image src={Logo} alt="logo image" className='h-10 w-[7rem] md:w-[10rem]' />
+                </Link>
 
-        <>
-            <nav className="w-full bg-slate-300 text-black fixed top-0 left-0 right-0 z-10">
-                <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
-                    <div>
-                        <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                            {/* LOGO */}
-                            <Link href="/">
-                                <Image src={Logo} alt="logo image" className='h-10 w-[10rem]' />
-                            </Link>
+                {/* HAMBURGER BUTTON FOR MOBILE */}
+                <div className="md:hidden">
+                    <button
+                        className="p-2 text-black rounded-md outline-none focus:border-gray-400 focus:border"
+                        onClick={() => setNavbar(!navbar)}
+                    >
+                        {navbar ? (
+                            <Image src={Close} width={15} height={15} alt="close icon" />
+                        ) : (
 
-                            {/* HAMBURGER BUTTON FOR MOBILE */}
-                            <div className="md:hidden">
-                                <button
-                                    className="p-2 text-black rounded-md outline-none focus:border-gray-400 focus:border"
-                                    onClick={() => setNavbar(!navbar)}
-                                >
-                                    {navbar ? (
-                                        <Image src={Close} width={15} height={15} alt="close ison" className='' />
-                                    ) : (
-                                        <FaBars
-                                            className="focus:border-none active:border-none"
-                                        />
-                                    )}
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div>
-                        <div
-                            className={`w-full flex-1 justify-self-center pb-3 md:block md:pb-0 md:mt-0 ${navbar ? 'p-12 md:p-0 block' : 'hidden'
-                                }`}
-                        >
-                            <div className="md:flex justify-between w-full">
-                                <ul className="h-screen md:h-auto items-center justify-center md:flex md:w-[35%]">
-
-                                    <li className="flex justify-center pb-6 md:pb-0 text-black py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                                        <Link href="" onClick={() => setNavbar(!navbar)} className='flex'>
-                                            Product
-                                        </Link>
-                                        <Image src={ArrowDown} alt="arrow down" className='ml-2 cursor-pointer mt-1' />
-                                    </li>
-
-
-                                    <li className="pb-6 md:pb-0 text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                                        <Link href="" onClick={() => setNavbar(!navbar)}>
-                                            Pricing
-                                        </Link>
-                                    </li>
-
-                                    <li className="pb-6 md:pb-0 text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                                        <Link href="/enterprise" onClick={() => setNavbar(!navbar)}>
-                                            Enterprise
-                                        </Link>
-                                    </li>
-
-                                    <li className="pb-6 md:pb-0 text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                                        <Link href="/" onClick={() => setNavbar(!navbar)}>
-                                            Support
-                                        </Link>
-                                    </li>
-
-
-                                    <div className="md:hidden">
-
-                                        <li className="pb-6 md:pb-0 text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                                            <Link href="" onClick={() => setNavbar(!navbar)}>
-                                                Contact Sales
-                                            </Link>
-                                        </li>
-
-                                        <li className="pb-6 md:pb-0 text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-
-                                            <Link href="/login" onClick={() => setNavbar(!navbar)}>
-                                                Login
-                                            </Link>
-                                        </li>
-
-                                    </div>
-                                </ul>
-
-
-                                <ul className="h-screen md:h-auto items-center justify-center md:flex md:w-[48%]">
-
-                                    <li className="pb-6 md:pb-0 text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                                        <Link href="" onClick={() => setNavbar(!navbar)}>
-                                            Contact Sales
-                                        </Link>
-                                    </li>
-
-                                    <li className="pb-6 md:pb-0 text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                                        <Link href="/login" onClick={() => setNavbar(!navbar)}>
-                                            Login
-                                        </Link>
-                                    </li>
-
-                                    <div className="bg-[#462686] flex items-center justify-around w-[39%] px-4 rounded-[100px] text-white">
-                                        <button className=''>Try For Free</button>
-                                        <Image src={ArrowRight} alt="arrow down" />
-                                    </div>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+                            <Image src={Menu} width={15} height={15} alt="menu icon" />
+                        )}
+                    </button>
                 </div>
-            </nav>
-        </>
+            </div>
+
+
+
+            <div
+                className={`w-full md:w-4/5 pb-3 md:pb-0 md:mt-0 md:flex h-14 justify-between items-center z-50 ${navbar ? 'bg-slate-300 w-full pt-10 px-10 md:pt-0 md:px-0 h-screen' : 'hidden'
+                    }`}
+            >
+
+                <ul className="h-screen md:h-auto items-center justify-center md:flex md:w-[45%]">
+
+                    <li className="flex justify-center pb-6 md:pb-0 text-black py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                        <Link href="" onClick={() => setNavbar(!navbar)} className='flex'>
+                            Product
+                        </Link>
+                        <Image src={ArrowDown} width={18} height={18} alt="arrow down icon" className='pl-2 cursor-pointer' />
+                    </li>
+
+                    <li className="pb-6 md:pb-0 text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                        <Link href="" onClick={() => setNavbar(!navbar)}>
+                            Pricing
+                        </Link>
+                    </li>
+
+
+                    <li className="pb-6 md:pb-0 text-black py-2 px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                        <Link href="/enterprise" onClick={() => setNavbar(!navbar)}>
+                            Enterprise
+                        </Link>
+                    </li>
+
+                    <li className="pb-6 md:pb-0 text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                        <Link href="" onClick={() => setNavbar(!navbar)}>
+                            Support
+                        </Link>
+                    </li>
+
+                    <div className="bg-[#462686] md:hidden flex items-center justify-around w-8/12 h-10 mt-5 m-auto px-4 rounded-[100px] text-white">
+                        <button className=''>Try For Free</button>
+                    </div>
+                </ul>
+
+                <ul className='h-screen md:h-auto items-center justify-center md:flex md:w-[45%]'>
+                    <li className="pb-6 md:pb-0 text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                        <Link href="" onClick={() => setNavbar(!navbar)}>
+                            Contact Sale
+                        </Link>
+                    </li>
+
+                    <li className="pb-6 md:pb-0 text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                        <Link href="/login" onClick={() => setNavbar(!navbar)}>
+                            Login
+                        </Link>
+                    </li>
+                </ul>
+
+                <div className="bg-[#462686] hidden md:flex md:justify-around items-center text-center py-2 w-3/12 md:w-2/12 rounded-[100px] text-white">
+                    <button className='m-auto text-center'>Try For Free</button>
+                    <Image src={ArrowRight} width={18} height={18} alt="arrow right icon" className='pr-2' />
+                </div>
+
+            </div>
+        </nav>
     );
 }
 
